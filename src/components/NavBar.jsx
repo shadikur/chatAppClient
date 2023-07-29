cbf52e38bf0b25db98df546d493ecb3f9d1c100e
Mohammad Rahman <me@shadikur.com>
2023-07-29 20:10:31 +0200

    Navbar, Login & Register form has been added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar bg="dark" className='mb-4' style={{ height: "3.7rem" }}>
-            <h2 className='container'>
-                <Link to={"/"}>
-                    ChatApp
-                </Link>
-            </h2>
+            <div className='container'>
+                <h2 >
+                    <Link to={"/chat"} className='text-light text-decoration-none'>
+                        ChatApp
+                    </Link>
+                </h2>
+                <span className='text-warning'>Logged in as Shadikur</span>
+                <Nav>
+                    <Stack direction='horizontal' gap="3">
+                        <Link to={"/login"} className='text-light text-decoration-none'>
+                            Login
+                        </Link>
+                        <Link to={"/register"} className='text-light text-decoration-none'>
+                            Register
+                        </Link>
+                    </Stack>
+                </Nav>
+            </div>
         </Navbar>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Alert, Button, Col, Form, Row, Stack } from 'react-bootstrap';
 
 const Login = () => {
     return (
         <div>
-            Login
+            <Form>
+                <Row style={{
+                    height: "100vh",
+                    justifyContent: "center",
+                    paddingTop: "10%"
+                }}>
+                    <Col xs={6}>
+                        <Stack gap="3">
+                            <h2>Login</h2>
+                            <Form.Control type='email' placeholder='Email' />
+                            <Form.Control type='password' placeholder='Password' />
+                            <Button variant='primary' type='submit'>
+                                Login
+                            </Button>
+
+                            <Alert variant='danger'>
+                                <p>An error has been occured</p>
+                            </Alert>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Form>
         </div>
     );
 };
