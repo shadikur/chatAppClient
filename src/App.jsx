ae9efbdca4b16c4d1533d0016456dbee80ff7773
Mohammad Rahman <me@shadikur.com>
2023-07-29 19:30:07 +0200

    Route Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import io from 'socket.io-client';
+
+
 
 const App = () => {
-  const socket = io('http://localhost:3000');
-  socket.on('connect', () => {
-    console.log('connected');
-  });
   return (
     <div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Chat from './pages/Chat'
+import App from './App'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+
+const routes = createBrowserRouter([{
+  path: "/",
+  element: <Chat />,
+  errorElement: <h2>Page Not Found</h2>,
+},
+{
+  path: "/register",
+  element: <Register />
+},
+{
+  path: "/login",
+  element: <Login />
+},
+{
+  path: "*",
+  element: <Chat />
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <RouterProvider router={routes} >
     <App />
-  </React.StrictMode>,
+  </RouterProvider>
 )
